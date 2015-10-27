c4c7bafd9cc0aabd78e4b894140c99ca861b2868
Niek Eichner <niek1993@hotmail.com>
2015-10-27 16:33:04 +0100

    Default random config values

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+var config = {
+	port: 8080,
+	database: {
+		host 		 : "localhost",
+		user 		 : "root",
+		password : "root",
+		database : "fundispenser_local",
+		port		 : 8889
+	}
+};
+
+module.exports = config;
