3364c6034fdfcaaa0811ce7df345b8631debb31f
Horofic <ikweetnie123@hotmail.com>
2015-12-16 18:02:02 +0100

    Fixed http request and added build.bat

--- rpi/js/requestParser.js
+++ rpi/js/requestParser.js
@@ -1,6 +1,6 @@
 var request = require('request-json'),
-    client  = request.createClient('188.166.126.18:8080/');
+    client  = request.createClient('http://188.166.126.18:8080/');
 
 client.get('players/', function(err, res, body) {
-  console.log(err, res, body);
+  console.log(body);
 });
