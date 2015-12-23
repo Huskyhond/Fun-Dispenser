b142cc297fc6cfff6de415846f6fbee3206834fd
Horofic <ikweetnie123@hotmail.com>
2015-12-23 03:34:48 +0100

    First draft GUI

--- rpi/js/requestParser.js
+++ rpi/js/requestParser.js
@@ -2,5 +2,6 @@ var request = require('request-json'),
     client  = request.createClient('http://188.166.126.18:8080/');
 
 client.get('players/', function(err, res, body) {
-  console.log(body);
+	$(".text-muted").text("Hallo, "+body.error.code+"!");
+	console.log(body.error.code);
 });
