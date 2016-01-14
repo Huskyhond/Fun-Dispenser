b34a674c96467156cd358d74565e0c2cef88eb74
Niek Eichner <niek1993@hotmail.com>
2016-01-14 11:32:11 +0100

    added dashes, failsafe answers (not all imp yet)

--- server/database.js
+++ server/database.js
@@ -257,15 +257,15 @@ var database = {
 
 		if(!options.result) options.result = {};
 
-		if(!options.question || !options.question.questionId)
+		if(!options.question || !options.question.questionId) {
 			options.result = database.defaultError(102, "Variable 'questionId' not set, in object 'question'");
 			return callback(options.result);
 		}
-		if(!options.answer || !options.answer.answerId)
+		if(!options.answer || !options.answer.answerId)  {
 			options.result = database.defaultError(102, "Variable 'answerId' not set, in object 'answer'");
 			return callback(options.result);
 		}
-		if(!options.player || !options.player.playerId)
+		if(!options.player || !options.player.playerId) {
 			options.result = database.defaultError(102, "Variable 'playerId' not set, in object 'player'");
 			return callback(options.result);
 		}
